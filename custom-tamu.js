eb762f5c255a59f5fb3d54700b8fcf62d97f48bc
ujiteh <afauji07@gmail.com>
2023-07-29 00:14:59 +0700

    first commit

---
+++ custom-tamu.js
@@ -0,0 +1,13 @@
+const myKeysValues = window.location.search;
+
+const urlParams = new URLSearchParams(myKeysValues);
+
+const param = urlParams.get("to");
+
+// console.log("Kepada:",param);
+
+if (param === null || !param) {
+  document.write("Penerima Undangan");
+} else {
+  document.write(param);
+}
